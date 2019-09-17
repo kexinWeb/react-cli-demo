a8d3dd7c25fb6d33b55475b779401b7832c62a26
kekekekeke <1657619661@qq.com>
2019-09-17 10:58:11 +0800

    FEAT：测试route

--- src/const.js
+++ src/const.js
@@ -2,4 +2,4 @@ import path from 'path'
 export const initSrcDir = path.resolve(__dirname, '../boilerplates/**/*')
 export const initDestDir = path.resolve(process.cwd(), './output')
 export const addSrcDir = path.resolve(__dirname, '../boilerplates/**/*')
-export const addDestDir = path.resolve(__dirname, '../output/routes')
+export const addDestDir = path.resolve(process.cwd(), './output/routes')
